f572e4469c67af4156985461563b0db31e81dca1
Dattatrya More <dattatrya.more@transformco.com>
2025-07-11 16:58:46 +0000

    added sample scripts to replciate browserstack session merge issue

--- tests/spec/example-pom-fixture.spec.ts
+++ tests/spec/example-pom-fixture.spec.ts
@@ -1,6 +1,7 @@
-import { basePage as test } from "../fixture/basePage";
+import { expect, basePage as test } from "../fixture/basePage";
 
-test("has title", async ({ homePage }) => {
+
+test("e2e test - 1", async ({ homePage }) => {
   await homePage.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
@@ -8,7 +9,7 @@ test("has title", async ({ homePage }) => {
   await homePage.verify();
 });
 
-test("get started link", async ({ page, homePage, installationPage }) => {
+test("e2e test - 2", async ({ page, homePage, installationPage }) => {
   await homePage.goto("https://playwright.dev/");
 
   // Click the get started link.
@@ -18,4 +19,27 @@ test("get started link", async ({ page, homePage, installationPage }) => {
   // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 
   await installationPage.verify();
+});
+
+test("e2e test - 3 (failing)", async ({ homePage }) => {
+  await homePage.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  //await expect(page).toHaveTitle(/Playwright/);
+  await homePage.verify();
+  expect(false, "marking test failure for reporting").toBeTruthy(); // This will fail
+
+});
+
+test("e2e test - 4 (failing)", async ({ page, homePage, installationPage }) => {
+  await homePage.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+  await installationPage.verify();
+  expect(false, "marking test failure for reporting").toBeTruthy(); // This will fail
 });
--- tests/spec/example-without-fixture.spec.ts
+++ tests/spec/example-without-fixture.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('without-fixure e2e test - 1', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('without-fixure e2e test - 2', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
@@ -15,4 +15,17 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+
+test('without-fixure e2e test - 3 (failing test)', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  expect(false, "marking test failure for reporting in dash").toBeTruthy(); // This will fail
+
 });
