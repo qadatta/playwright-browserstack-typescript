835c3893a60ad26bd27dac47ff668117ad696dfe
Dattatrya More <dattatrya.more@transformco.com>
2025-07-11 17:43:04 +0000

    updated github workflow

--- tests/spec/example-without-fixture.spec.ts
+++ tests/spec/example-without-fixture.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test';
+test.describe.configure({ mode: 'parallel' });
+
 
 test('without-fixure e2e test - 1', async ({ page }) => {
   await page.goto('https://playwright.dev/');
